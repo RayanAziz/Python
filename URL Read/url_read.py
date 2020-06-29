7384282149d23abb58e7e7a1d3d065b9a2574648
Rayan Gary A <63548264+RayanGary@users.noreply.github.com>
2020-06-29 20:11:24 +0300

    Update url_read.py

--- URL Read/url_read.py
+++ URL Read/url_read.py
@@ -1,17 +1,16 @@
 # Used to make requests
 import requests
-# Alternative library for the same purpose
-from urllib.request import urlopen
 
 # The URL to read from
 URL = 'https://raw.githubusercontent.com/RayanGary/Python/master/URL%20Read/example.txt'
 
-# Fetch the content for use in the second library
-content2 = urlopen(URL)
-# Fetch the content from the URL using the first library then print it
+# Fetch the content from the URL
 content = requests.get(URL)
 
-# Print the content using the first library
+keyword = 'Hello'
+
+# Print the content
 print(content.text)
-# Decode the page in utf-8 then print it using the second library
-print(content2.read().decode('utf8'))
+
+if keyword in content.text:
+   print("Keyword detected.")
