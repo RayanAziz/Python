4f1f39fef68e20c1b07d3223a64c7ef1229304a1
Rayan Gary A <63548264+RayanGary@users.noreply.github.com>
2020-06-29 17:32:01 +0300

    Update url_read.py

--- URL Read/url_read.py
+++ URL Read/url_read.py
@@ -1,8 +1,15 @@
-#Used to make requests
+# Used to make requests
+import requests
+# Alternative library for the same purpose
 from urllib.request import urlopen
 
 # The URL to read from
-URL = urlopen('https://raw.githubusercontent.com/RayanGary/Python/master/URL%20Read/example.txt')
+URL = 'https://raw.githubusercontent.com/RayanGary/Python/master/URL%20Read/example.txt'
+URL2 = urlopen(URL)
 
-# Decode the page in utf-8 and print it
-print(URL.read().decode('utf8'))
+# Fetch the content from the URL using the first library then print it
+content = requests.get(URL)
+print(content.text)
+
+# Decode the page in utf-8 then print it
+print(URL2.read().decode('utf8'))
