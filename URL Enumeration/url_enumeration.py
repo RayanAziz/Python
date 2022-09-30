6ef39b2958e68228e310e425d72e1ce0c770de05
Rayan Aziz <63548264+RayanAziz@users.noreply.github.com>
2022-09-30 14:26:15 +0300

    Update url_enumeration.py

--- URL Enumeration/url_enumeration.py
+++ URL Enumeration/url_enumeration.py
@@ -1,10 +1,10 @@
-# I wrote this script to prove that mecai.org is just a shell organization that charges $100 to issue fake certificates
+# I wrote this script to prove that mecai.net is just a shell organization that charges $100 to issue fake certificates
 # that can be easily mass extracted from their website and traced back to one real company in Saudi Arabia.
 
 import requests
 
 # Base URL
-URL = "https://www.mecai.org/cr/"
+URL = "https://www.mecai.net/cr/"
 
 # Counter for certificate groups found
 count = 0
@@ -13,11 +13,13 @@ print(URL)
 
 # Try numbers 0 to 1000 in the URL
 for cr in range (0, 1000):
-	URL += str(cr)+".php"
-	content = requests.get(URL)
+	link = URL
+	link += str(cr)+".php"
+	content = requests.get(link)
 	keyword = 'Download'
-
+	print(link)
+    
 	if keyword in content.text:
-		print("Certificate group found: " + URL)
+		print("Certificate group found: " + link)
 		count = count + 1
 print ("Number of certificate groups found: ", count)
